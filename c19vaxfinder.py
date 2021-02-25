a3e8c6bca181b720d17b46fe6d03358dff223096
DC <dc@technoexile.com>
2021-02-24 22:27:50 -0700

    quick and dirty support for two Pharmaca locations in Boulder known to have the vaccine

--- c19vaxfinder.py
+++ c19vaxfinder.py
@@ -38,6 +38,22 @@ WALGREENS_URI         = 'https://www.walgreens.com/findcare/vaccination/covid-19
 DISCORD_AVATAR        = 'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/1800x1200_virus_3d_render_red_03_other.jpg?resize=*:350px'
 DISCORD_ICON          = 'http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/syringe.png'
 DISCORD_NICK          = 'C19'
+PHARMACA_REQ_URI      = 'https://pharmaca.as.me/schedule.php?action=showCalendar&fulldate=1&owner=20105611&template=weekly'
+PHARMACA_BASEURI      = 'https://pharmaca.as.me/'
+pharmacaLocations = [
+        {
+            'name'      : "tablemesa",
+            'calendar'  : 4602706,
+            'calendarID': 4222381,
+            'uri'       : PHARMACA_BASEURI + "tablemesa"
+        },
+        {
+            'name'      : "broadway",
+            'calendar'  : 4490121,
+            'calendarID': 4222367,
+            'uri'       : PHARMACA_BASEURI + "broadway"
+        }
+    ]
 
 http_headers = {
         'Content-Type': 'application/json',
@@ -51,7 +67,7 @@ def readFile(filename):
     obj = json.loads(data)
     return(obj)
 
-def checkAvailability(locations):
+def checkWalgreensAvailability(locations):
     
     for location in locations:
         
@@ -80,19 +96,19 @@ def checkAvailability(locations):
         data = response.json()
         isAvailable = data['appointmentsAvailable']
         if isAvailable:
-            print("Good news, there is an appointment available at %s, ZIP %s" % (location['name'], location['zip']) )
-            discordEmbed(location, DISCORD_WEBHOOK)
+            print("Good news, there is an appointment available at Walgreens %s, ZIP %s" % (location['name'], location['zip']) )
+            discordEmbed(location, "Walgreens", WALGREENS_URI, DISCORD_WEBHOOK)
         else:
             print("Sorry, no appointment available at %s" % location['name'])
         
-def discordEmbed(data, discord_uri):
-    print("Sending alert data to discord at %s" % (discord_uri))
+def discordEmbed(data, provider, provider_uri, discord_uri):
+    print("Sending alert data for provider_uri %s to discord at %s" % (provider_uri, discord_uri))
     http_headers = {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
     }
 
-    description = "C19 vaccination appointment available for %s" % data['name']
+    description = "C19 vaccination appointment available at %s for %s" % (provider, data['name'])
     
     payload = {
         "username": DISCORD_NICK,
@@ -103,26 +119,51 @@ def discordEmbed(data, discord_uri):
                 "author": 
                     {
                         "name"      : "C19 Vaccine",
-                        "url"       : WALGREENS_URI,
+                        "url"       : provider_uri,
                         "icon_url"  : DISCORD_ICON
                     },            
 
                 "title"      : "Time to get your shot!",
-                "url"        : WALGREENS_URI,
+                "url"        : provider_uri,
                 "description": description,
                 "color"      :  15258703,
                 "thumbnail"  :
                     {
-                        "url": WALGREENS_URI
+                        "url": provider_uri
                     },
                 }
             ]
         }
     response = http.post(discord_uri, headers=http_headers, json=payload)
 
+def checkPharmaca(locations):
+    #print(location['name'])
+    #print(location['calendar'])
+    #print(location['calendarID'])
+    #print(location['uri'])
+    http_headers = {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    }
+    for location in locations:
+        print("checking Pharmaca availability in %s" % location['name'])
+        payload = "type=19573151&calendar=" + str(location['calendar']) + "&skip=true&options%5Bqty%5D=1&options%5BnumDays%5D=5&ignoreAppointment=&appointmentType=&calendarID=" + str(location['calendarID'])
+        response = http.post(PHARMACA_REQ_URI, headers = http_headers, data = payload)
+        data = response.text
+        if data.find("No times are available"):
+            print("no times found. bum deal dude")
+        else:
+            print("woot! found an appointment at Pharmaca on %s" % location['name'])
+            discordEmbed(location, "Pharmaca", PHARMACA_BASEURI+location['name'], DISCORD_WEBHOOK)
+            
+
+
 def main():
-    locations = readFile(LATLONG_FILE)
-    checkAvailability(locations)
+    #read walgreens lat longs from file
+    walgreensLocations = readFile(LATLONG_FILE)
+    # check lat longs against walgreens API
+    checkWalgreensAvailability(walgreensLocations)
+    # check Pharmaca availability
+    checkPharmaca(pharmacaLocations)
         
 
 if __name__ == "__main__":
