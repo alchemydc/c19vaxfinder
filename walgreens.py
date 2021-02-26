ca8445e37c1c2d97125d51bb966915fb8f14f792
DC <dc@technoexile.com>
2021-02-26 10:10:52 -0700

    remove redundant deps

--- walgreens.py
+++ walgreens.py
@@ -2,14 +2,6 @@
 # -*- coding: utf-8 -*-
 
 # libs
-import requests
-from requests_toolbelt.utils import dump
-import json
-import os
-import sys
-from dotenv import load_dotenv
-from pathlib import Path
-from datetime import date
 from c19vaxfinder import *
 
 def main():
